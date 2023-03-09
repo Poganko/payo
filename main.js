8ae83bafd437cf3704ff5671f0acc29d2d2c8749
Stanislau <bender67@gmail.com>
2023-03-09 22:12:07 +0300

    1

--- main.js
+++ main.js
@@ -8,7 +8,12 @@
 //     .then((data) => console.log(data))
 //     .catch((error) => console.error(error));
 
-fetch("https://tools.payonstories.com/api/pc/item?id=4211&name=4211")
+fetch(
+    "https://tools.payonstories.com/api/pc/item?id=4211&name=4211",
+    {
+        mode: "no-cors",
+    }
+)
     .then(function (response) {
         // The API call was successful!
         return response.json();
