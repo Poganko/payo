b245b6999cc66a89ea9f84e19fc6a10a9f24d891
Stanislau <bender67@gmail.com>
2023-03-09 22:05:19 +0300

    1

---
+++ main.js
@@ -0,0 +1,23 @@
+// fetch("https://tools.payonstories.com/api/pc/item?id=4211&name=4211")
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         return response.json();
+//     })
+//     .then((data) => console.log(data))
+//     .catch((error) => console.error(error));
+
+fetch("https://tools.payonstories.com/api/pc/item?id=4211&name=4211")
+    .then(function (response) {
+        // The API call was successful!
+        return response.json();
+    })
+    .then(function (data) {
+        // This is the JSON from our response
+        console.log(data);
+    })
+    .catch(function (err) {
+        // There was an error
+        console.warn("Something2 went wrong.", err);
+    });
